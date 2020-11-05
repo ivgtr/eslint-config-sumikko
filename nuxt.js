ffbe526a75ba909d8e0c2bbe428549d54da31c33
ivgtr <gear.1112t.o@gmail.com>
2020-11-05 16:11:50 +0900

    :sparkles: add: nuxtのルールを追加

---
+++ nuxt.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    './meta.js'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  plugins: ['prettier'],
+  rules: {}
+}
