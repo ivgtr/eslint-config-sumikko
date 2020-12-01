19258f65695bdf468482155e7d5ca1b57715b04d
ivgtr <gear.1112t.o@gmail.com>
2020-12-01 13:06:18 +0900

    :sparkles: add: ルールを追加

---
+++ pritter.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/unicorn'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      require('./.prettierrc.json'),
+      { usePrettierrc: false }
+    ]
+  }
+}
