e54d19858b66c8fce309d7bab598b77f3ade3b09
ivgtr <ivgtr.me@gmail.com>
2021-04-30 00:46:40 +0900

    :sparkles: change: import周りの修正を取り込む

--- module.js
+++ module.js
@@ -6,26 +6,46 @@ module.exports = {
   },
   plugins: ['import'],
   rules: {
-    // Static analysis
-    'import/default': 2,
-    'import/named': 2,
-    'import/namespace': 2,
-    'import/no-absolute-path': 2,
-    'import/no-self-import': 2,
-    'import/no-unresolved': 2,
+    // for both TypeScript and non-TypeScript rules
+
+    // ** Static analysis **
     'import/no-useless-path-segments': 2,
 
-    // Helpful warnings
-    'import/export': 2,
-    'import/no-named-as-default': 2,
-    // "import/no-named-as-default-member": 2,
-
-    // Style guide
+    // ** Style guide **
     // "always" for Native ESM, "never" for CJS/TS
     // "import/extensions": 2,
     'import/first': 2,
     'import/newline-after-import': 2,
     'import/no-duplicates': 2,
-    'import/order': 2
-  }
+    'import/order': [
+      2,
+      {
+        groups: [
+          ['builtin', 'external', 'internal'],
+          ['index', 'parent', 'sibling']
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }
+    ]
+  },
+  overrides: [
+    {
+      // for non-TypeScript files
+      files: ['*.js', '*.jsx', '*.mjs'],
+      rules: {
+        // ** Static analysis **
+        'import/default': 2,
+        'import/named': 2,
+        'import/namespace': 2,
+        'import/no-absolute-path': 2,
+        'import/no-self-import': 2,
+        'import/no-unresolved': 2,
+
+        // ** Helpful warnings **
+        'import/export': 2,
+        'import/no-named-as-default': 2
+        // "import/no-named-as-default-member": 2,
+      }
+    }
+  ]
 }
